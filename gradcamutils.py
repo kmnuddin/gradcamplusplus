d1c02547f698eaea9bf864187b5bc5fcf34a9e7a
kmnuddin <kmnuddin@memphis.edu>
2020-06-02 13:49:25 -0500

    RT_100 gradcam

--- gradcamutils.py
+++ gradcamutils.py
@@ -1,22 +1,27 @@
 from scipy.ndimage.interpolation import zoom
 import numpy as np
 
-from keras.backend import tensorflow_backend
-from keras import backend as K
-from keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras import backend as K
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+import tensorflow as tf
+
+tf.compat.v1.disable_eager_execution()
 
 import matplotlib.pyplot as plt
 
-def grad_cam(input_model, image, layer_name,H=224,W=224):
-    cls = np.argmax(input_model.predict(image))
-    def normalize(x):
+def normalize(x):
         """Utility function to normalize a tensor by its L2 norm"""
         return (x + 1e-10) / (K.sqrt(K.mean(K.square(x))) + 1e-10)
+
+def grad_cam(input_model, image, layer_name,H=224,W=224):
+    cls = np.argmax(input_model.predict(image))
+    
     """GradCAM method for visualizing input saliency."""
     y_c = input_model.output[0, cls]
     conv_output = input_model.get_layer(layer_name).output
     grads = K.gradients(y_c, conv_output)[0]
-    #grads = normalize(grads)
+    grads = normalize(grads)
     gradient_function = K.function([input_model.input], [conv_output, grads])
 
     output, grads_val = gradient_function([image])
@@ -24,11 +29,12 @@ def grad_cam(input_model, image, layer_name,H=224,W=224):
 
     weights = np.mean(grads_val, axis=(0, 1))
     cam = np.dot(output, weights)
-    #print (cam)
-
+#     normalize(cam)
+    
     cam = np.maximum(cam, 0)
-    #cam = resize(cam, (H, W))
+#     cam = np.resize(cam, (H, W))
     cam = zoom(cam,H/cam.shape[0])
+    print (cam.shape)
     #cam = np.maximum(cam, 0)
     cam = cam / cam.max()
     return cam
@@ -40,7 +46,7 @@ def grad_cam_plus(input_model, img, layer_name,H=224,W=224):
     conv_output = input_model.get_layer(layer_name).output
     #conv_output = target_conv_layer, mixed10の出力1,5,5,2048
     grads = K.gradients(y_c, conv_output)[0]
-    #grads = normalize(grads)
+    grads = normalize(grads)
 
     first = K.exp(y_c)*grads
     second = K.exp(y_c)*grads*grads
@@ -64,7 +70,7 @@ def grad_cam_plus(input_model, img, layer_name,H=224,W=224):
     deep_linearization_weights = np.sum((weights*alphas).reshape((-1,conv_first_grad[0].shape[2])),axis=0)
     #print deep_linearization_weights
     grad_CAM_map = np.sum(deep_linearization_weights*conv_output[0], axis=2)
-
+#     normalize(grad_CAM_map)
     # Passing through ReLU
     cam = np.maximum(grad_CAM_map, 0)
     cam = zoom(cam,H/cam.shape[0])
